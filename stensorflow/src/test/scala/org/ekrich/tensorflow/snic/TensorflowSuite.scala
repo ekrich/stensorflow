3dc3398e494af98f82861e2d4ff2087b732d9f67
Eric K Richardson <ekrichardson@gmail.com>
2019-07-08 17:38:09 -0700

    Progress on example

--- stensorflow/src/test/scala/org/ekrich/tensorflow/snic/TensorflowSuite.scala
+++ stensorflow/src/test/scala/org/ekrich/tensorflow/snic/TensorflowSuite.scala
@@ -3,8 +3,23 @@ package org.ekrich.tensorflow.snic
 import scalanative.unsafe._
 import utest._
 import org.ekrich.tensorflow.snic.tensorflow._
+import org.ekrich.tensorflow.snic.tensorflowEnums._
+import scalanative.libc.stdlib._
+import scalanative.unsafe.CFuncPtr3
 
 object TensorflowSuite extends TestSuite {
+
+  // def DeallocateTensor(data: Ptr[Byte], sz: CSize, unk: Ptr[Byte]): Unit = {
+  //   free(data)
+  //   println("Deallocate tensor")
+  // }
+
+  val DeallocateTensor = new CFuncPtr3[Ptr[Byte], CSize, Ptr[Byte], Unit] {
+    def apply(data: Ptr[Byte], sz: CSize, unk: Ptr[Byte]): Unit = {
+      //free(data)
+      println("Free Tensor")
+    }
+  }
 
   val tests = this {
     'TF_Version {
@@ -17,12 +32,21 @@ object TensorflowSuite extends TestSuite {
       Zone { implicit z =>
         println("Running example...")
         assert("1.13.1" == fromCString(TF_Version()))
+
+        // handle dims
+        val dimsVals = Seq(1, 5, 12)
+        val dimsSize = dimsVals.size
+        val dims     = alloc[int64_t](sizeof[int64_t] * dimsSize)
+        for (i <- 0 until dimsSize) {
+          dims(i) = dimsVals(i)
+        }
+
+        // handle data based on dims
         var dataSize = sizeof[CFloat]
-        val dims     = Seq(1, 5, 12)
-        for (i <- dims) dataSize *= i
+        for (i <- dimsVals) dataSize *= i
         println(s"data_size: $dataSize")
 
-        def data = alloc[CFloat](dataSize)
+        val data = alloc[CFloat](dataSize)
 
         val dataVals = Seq(
           -0.4809832f, -0.3770838f, 0.1743573f, 0.7720509f, -0.4064746f,
@@ -38,13 +62,51 @@ object TensorflowSuite extends TestSuite {
           0.1748378f, 0.7718275f, -0.4073670f, 0.0107582f, 0.0062978f,
           0.9131795f, 0.7187147f, -0.0394935f, 0.1184392f, -0.6840039f
         )
-
+        // copy to memory
         for (i <- 0 until dataVals.size) {
           data(i) = dataVals(i)
         }
 
+        println(dimsVals)
+        println(dims)
+
         println(dataVals)
         println(data)
+
+        // /**
+        //  * Return a new tensor that holds the bytes data[0,len-1].
+        //  *
+        //  *  The data will be deallocated by a subsequent call to TF_DeleteTensor via:
+        //  *       (*deallocator)(data, len, deallocator_arg)
+        //  *  Clients must provide a custom deallocator function so they can pass in
+        //  *  memory managed by something like numpy.
+        //  *
+        //  *  May return NULL (and invoke the deallocator) if the provided data buffer
+        //  *  (data, len) is inconsistent with a tensor of the given TF_DataType
+        //  *  and the shape specified by (dima, num_dims).
+        //  */
+        // def TF_NewTensor(
+        //     value: TF_DataType,
+        //     dims: Ptr[int64_t],
+        //     num_dims: CInt,
+        //     data: Ptr[Byte],
+        //     len: CSize,
+        //     deallocator: CFuncPtr3[Ptr[Byte], CSize, Ptr[Byte], Unit],
+        //     deallocator_arg: Ptr[Byte]): Ptr[TF_Tensor] = extern
+
+        val nul = alloc[Byte]
+        !nul = 0x00
+
+        val tensor = TF_NewTensor(TF_FLOAT,
+                                  dims,
+                                  dimsSize,
+                                  data.asInstanceOf[Ptr[Byte]],
+                                  dataSize,
+                                  DeallocateTensor,
+                                  nul);
+
+        println(s"Tensor: $tensor")
+        TF_DeleteTensor(tensor)
         println("Done.")
       }
     }
