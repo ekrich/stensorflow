85291c77f0b829240e5608546344223a3e841cc6
Eric K Richardson <ekrichardson@gmail.com>
2022-11-28 10:37:47 -0800

    Fix formatting

--- stensorflow/src/test/scala/org/ekrich/tensorflow/unsafe/TensorflowTest.scala
+++ stensorflow/src/test/scala/org/ekrich/tensorflow/unsafe/TensorflowTest.scala
@@ -27,7 +27,8 @@ class TensorflowTest {
     Zone { implicit z =>
       val reportVersion = fromCString(TF_Version())
       println(s"Tensorflow version: ${reportVersion}")
-      assertTrue(s"Looking for version: $tfVersion", reportVersion.startsWith(tfVersion))
+      assertTrue(s"Looking for version: $tfVersion",
+                 reportVersion.startsWith(tfVersion))
     }
   }
   @Test def TF_ExampleTest(): Unit = {
